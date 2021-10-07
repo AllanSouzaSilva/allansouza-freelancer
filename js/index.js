59d97764271fa78a102cc6acc830ce8ef47ca55c
AllanSouzaSilva <allan-souza16@hotmail.com>
2021-10-07 17:58:44 -0300

    Melhorias no SEO

--- js/index.js
+++ js/index.js
@@ -90,3 +90,8 @@ function generatePDF() {
     .save();
 
 }
+
+window.addEventListener("scroll", function () {
+  var nav = this.document.querySelector("nav");
+  nav.classList.toggle("sticky", window.scrollY > 0);
+});
