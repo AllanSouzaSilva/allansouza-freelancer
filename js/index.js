e6bdff72ccb273e14cb2dc061353ea9959e36851
AllanSouzaSilva <allan-souza16@hotmail.com>
2021-10-07 18:16:57 -0300

    Adicionando efeito scrollreveal Js

--- js/index.js
+++ js/index.js
@@ -4,80 +4,98 @@ window.sr = ScrollReveal();
 
 sr.reveal(".container-descricao-img", {
   duration: 3000,
-  origin: "bottom",
-  distance: "-100px",
+  origin: "top",
+  distance: "100px",
 });
+sr.reveal(".paragrafo-profissional", {
+  duration: 3000,
+  origin: "top",
+  distance: "100px",
+});
+sr.reveal(".icon-hobbie", {
+  duration: 3000,
+  origin: "top",
+  distance: "100px",
+});
+sr.reveal(".paragrafo-tecnologia", {
+  duration: 3000,
+  origin: "top",
+  distance: "100px",
+});
+
+
+
 sr.reveal(".hero-img", {
   duration: 3000,
-  origin: "bottom",
-  distance: "-100px",
+  origin: "top",
+  distance: "100px",
 });
 sr.reveal(".container-img-perfil", {
   duration: 3000,
-  origin: "left",
-  distance: "400px",
+  origin: "top",
+  distance: "100px",
 });
 sr.reveal(".secao-titulo", {
   duration: 3000,
-  origin: "bottom",
-  distance: "400px",
+  origin: "top",
+  distance: "100px",
 });
 sr.reveal(".paragrafo-hobbie", {
   duration: 3000,
-  origin: "bottom",
-  distance: "400px",
+  origin: "top",
+  distance: "100px",
 });
 sr.reveal(".container-icon", {
   duration: 3000,
   origin: "top",
-  distance: "400px",
+  distance: "100px",
 });
 sr.reveal(".paragrafo-hobbie", {
-  duration: 4000,
+  duration: 3000,
   origin: "top",
-  distance: "400px",
+  distance: "100px",
 });
 sr.reveal(".container-paragrafo", {
   duration: 3000,
   origin: "top",
-  distance: "400px",
+  distance: "100px",
 });
 sr.reveal(".container-videos", {
   duration: 3000,
   origin: "top",
-  distance: "400px",
+  distance: "100px",
 });
 sr.reveal(".descricao-por", {
   duration: 3000,
-  origin: "bottom",
-  distance: "400px",
+  origin: "top",
+  distance: "100px",
 });
 sr.reveal(".container-rodape", {
   duration: 3000,
   origin: "top",
-  distance: "600px",
+  distance: "100px",
 });
 
 sr.reveal(".container-redesociais", {
   duration: 3000,
   origin: "top",
-  distance: "400px",
+  distance: "100px",
 });
 
 sr.reveal(".contato1", {
   duration: 3000,
-  origin: "bottom",
-  distance: "400px",
+  origin: "top",
+  distance: "100px",
 });
 sr.reveal(".contato2", {
   duration: 3000,
-  origin: "bottom",
-  distance: "400px",
+  origin: "top",
+  distance: "100px",
 });
 sr.reveal(".contato3", {
   duration: 3000,
-  origin: "bottom",
-  distance: "400px",
+  origin: "top",
+  distance: "100px",
 });
 
 
